f9a4d6393e9c76141a43d90c865a27de371e61ce
AntarNath <antarnathbd@gmail.com>
2022-08-13 14:53:54 +0600

    first commit

---
+++ Problem-01.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a, a + n);
+    cout << a[k - 1] << " ";
+    for (int i = 0, j = n - 1; i < j; i++, j--)
+    {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    cout << a[k - 1];
+
+    return 0;
+}
